9907099b544c2b4fd75690f0cb014a5b431479ad
diogom-42 <diogomoura167@gmail.com>
2018-05-23 08:52:46 -0300

    Adicionando arquivos _START_

--- www/js/index.js
+++ www/js/index.js
@@ -1,24 +1,56 @@
-var resultDiv;
+var resultMac;
+var resultSerial;
+
 
 document.addEventListener("deviceready", init, false);
+
 function init() {
-	document.querySelector("#startScan").addEventListener("touchend", startScan, false);
-	resultDiv = document.querySelector("#results");
+	document.querySelector("#mac").addEventListener("touchend", startScan, false);
+    resultMac = document.querySelector("#mac");
+
 }
 
 function startScan() {
 
 	cordova.plugins.barcodeScanner.scan(
 		function (result) {
-			var s = "Result: " + result.text + "<br/>" +
-			"Format: " + result.format + "<br/>" +
-			"Cancelled: " + result.cancelled;
-			resultDiv.innerHTML = s;
+			var s = result.text
+            resultMac.innerHTML = s;
+            alert(s);
+            $('#mac').val(s);
+
 		}, 
 		function (error) {
 			alert("Scanning failed: " + error);
 		}
-	);
+    );
+
+
+}
+
+document.addEventListener("deviceready", init2, false);
+
+function init2() {
+
+    document.querySelector("#serial").addEventListener("touchend", startScan2, false);
+	resultSerial = document.querySelector("#serial");
+}
+
+function startScan2() {
+
+	cordova.plugins.barcodeScanner.scan(
+		function (result) {
+			var s2 = result.text
+            resultSerial.innerHTML = s2;
+            alert(s2);
+            $('#serial').val(s2);
+
+		}, 
+		function (error) {
+			alert("Scanning failed: " + error);
+		}
+    );
+
 
 }
 
